ca55db24b17784edb6ed335a531c7ea7dffe4c83
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-21 01:32:35 +0800

    Update Class03.java

--- Class03.java
+++ Class03.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 public class class03 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		int n,sum=0;
 		BufferedReader buf;
 		String str;
