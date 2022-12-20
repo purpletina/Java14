45c5d3308a2e3b10bc1b5c0e35f225afff2a3c26
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-21 01:40:33 +0800

    Create Class08.java

---
+++ Class08.java
@@ -0,0 +1,27 @@
+package myJava14;
+import java.io.*;
+public class class08 {
+	public static void main(String[] args) throws IOException {
+		FileReader fr1=new FileReader("c:\\java\\aaa.txt");
+		FileReader fr2=new FileReader("c:\\java\\bbb.txt");
+		FileWriter fw=new FileWriter("c:\\java\\ccc.txt");
+		BufferedReader bfr1=new BufferedReader(fr1);
+		BufferedReader bfr2=new BufferedReader(fr2);
+		BufferedWriter bfw=new BufferedWriter(fw);
+		String str;
+		
+		while((str=bfr1.readLine())!=null) {
+			bfw.write(str);
+			bfw.newLine();
+		}
+		while((str=bfr2.readLine())!=null) {
+			bfw.write(str);
+			bfw.newLine();
+		}
+		bfw.flush();
+		fw.close();
+		fr1.close();
+		fr2.close();
+	}
+
+}
